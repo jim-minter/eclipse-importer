1fbb462b13e61a04d72f914c08da5109a5c5919f
Jim Minter <jminter@redhat.com>
2015-09-02 21:43:00 +0100

    run full eclipse

--- src/com/redhat/importer/Application.java
+++ src/com/redhat/importer/Application.java
@@ -5,43 +5,55 @@ import java.net.URI;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.egit.core.op.ConnectProviderOperation;
-import org.eclipse.equinox.app.IApplication;
-import org.eclipse.equinox.app.IApplicationContext;
 import org.eclipse.m2e.core.MavenPlugin;
 import org.eclipse.m2e.core.project.IProjectConfigurationManager;
 import org.eclipse.m2e.core.project.ResolverConfiguration;
+import org.eclipse.ui.IStartup;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.PlatformUI;
 
-public class Application implements IApplication {
-	public Object start(final IApplicationContext context) throws Exception {
-		String[] args = (String[])context.getArguments().get("application.args");
+public class Application implements IStartup {
+	public void earlyStartup() {
+		try {
+			String[] args = Platform.getApplicationArgs();
 
-		IProgressMonitor pm = new ProgressMonitor();
+			IProgressMonitor pm = new ProgressMonitor();
 
-		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-		IProject project = root.getProject(args[0]);
-		IProjectDescription desc = ResourcesPlugin.getWorkspace().newProjectDescription(args[0]);
-		desc.setLocationURI(new URI("file://" + args[1]));
-		project.create(desc, pm);
-		project.open(pm);
+			IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+			IProject project = root.getProject(args[0]);
+			IProjectDescription desc = ResourcesPlugin.getWorkspace()
+					.newProjectDescription(args[0]);
+			desc.setLocationURI(new URI("file://" + args[1]));
+			project.create(desc, pm);
+			project.open(pm);
 
-		ResolverConfiguration rc = new ResolverConfiguration();
-		rc.setResolveWorkspaceProjects(false);
-		IProjectConfigurationManager pcm = MavenPlugin.getProjectConfigurationManager();
-		pcm.enableMavenNature(project, rc, pm);
+			ResolverConfiguration rc = new ResolverConfiguration();
+			rc.setResolveWorkspaceProjects(false);
+			IProjectConfigurationManager pcm = MavenPlugin
+					.getProjectConfigurationManager();
+			pcm.enableMavenNature(project, rc, pm);
 
-		// project.build(IncrementalProjectBuilder.FULL_BUILD, pm);
+			project.build(IncrementalProjectBuilder.FULL_BUILD, pm);
 
-		ConnectProviderOperation op = new ConnectProviderOperation(project);
-		op.execute(pm);
+			ConnectProviderOperation op = new ConnectProviderOperation(project);
+			op.execute(pm);
 
-		System.out.println("Done.");
+			System.out.println("Done.");
 
-		return IApplication.EXIT_OK;
-	}
+			final IWorkbench workbench = PlatformUI.getWorkbench();
+			workbench.getDisplay().asyncExec(new Runnable() {
+				public void run() {
+					workbench.close();
+				}
+			});
 
-	public void stop() {
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 }
